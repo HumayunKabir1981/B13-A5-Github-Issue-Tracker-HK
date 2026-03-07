7df83caf7af88fea652896f3d1ba109979ab0020
HumayunKabir1981 <callmilton.com>
2026-03-08 00:24:16 +0600

    all data show in UI

---
+++ script/index.js
@@ -0,0 +1,60 @@
+const removeActive = () => {
+    const button = document.querySelectorAll(".js-btn")
+    button.forEach(btn => btn.classList.remove("btn-active"))
+
+}
+
+
+
+const btnAll = () => {
+    removeActive();
+    const clickBtn = document.getElementById('btn-all')
+    clickBtn.classList.add("btn-active")
+
+    loadGithub();
+
+}
+
+const btnOpen = () => {
+    removeActive();
+    const clickBtn = document.getElementById('btn-open')
+    clickBtn.classList.add("btn-active")
+}
+
+const btnClose = () => {
+    removeActive();
+    const clickBtn = document.getElementById('btn-close')
+    clickBtn.classList.add("btn-active")
+}
+
+
+
+
+
+const loadGithub = () => {
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            displayGithub(data.data)
+        });
+}
+
+
+
+const displayGithub = (githubs) => {
+    const githubContainer = document.getElementById('github-container');
+    githubContainer.innerHTML = "";
+    githubs.forEach(github => {
+        const githubCard = document.createElement('div');
+        githubCard.innerHTML = `
+      <div class="bg-white rounded-xl py-10 px-5 shadow-sm text-center space-y-4">
+      <h2 class="text-xl font-bold">${github.title}</h2>
+      <p>Meaning /Pronounciation</p>
+      <div>${github.description} / ${github.status}</div>
+
+      
+      `;
+        githubContainer.append(githubCard);
+    })
+}
