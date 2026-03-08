93afea66bab876b56fdcbfd415fc6a9e91eebf67
HumayunKabir1981 <callmilton.com>
2026-03-08 20:03:00 +0600

    all data show in UI almost done

--- script/index.js
+++ script/index.js
@@ -19,7 +19,7 @@ const btnOpen = () => {
     removeActive();
     const clickBtn = document.getElementById('btn-open')
     clickBtn.classList.add("btn-active")
-loadGithubOpen();
+    loadGithubOpen();
 
 }
 
@@ -47,25 +47,66 @@ const loadGithub = () => {
 
 
 const displayGithub = (githubs) => {
-    const issueCount=document.getElementById('issue-count')
-    issueCount.innerText=githubs.length;
+    const issueCount = document.getElementById('issue-count')
+    issueCount.innerText = githubs.length;
 
-    console.log(githubs.length);
-    
     const githubContainer = document.getElementById('github-container');
     githubContainer.innerHTML = "";
     githubs.forEach(github => {
+
         const githubCard = document.createElement('div');
         githubCard.innerHTML = `
-      <div class="bg-white rounded-xl py-10 px-5 shadow-sm text-center space-y-4">
-      <h2 class="text-xl font-bold">${github.title}</h2>
-      <p>Meaning /Pronounciation</p>
-      <div>${github.description} / ${github.status}</div>
+      <div class=" github-card bg-white w-full h-full rounded-xl py-10 px-5 shadow-sm text-center space-y-4 ">
 
+                <div class="flex justify-between items-center">
+                    <img class="h-10 w-10" src="./assets/Open-Status.png" alt="">
+                    <span class="bg-yellow-300 rounded-full py-1 px-6 text-sm font-semibold">${github.priority}</span>
+                </div>
+
+                <h2 class="text-xl font-bold">
+                    ${github.title}
+                </h2>
+                <p class="text-[#64748B]">${github.description} </p>
+
+                <div class="flex gap-3 items-start">
+                    <span class="bg-yellow-300 rounded-full py-1 px-5 flex gap-2 items-center text-sm">
+                        <i class="fa-solid fa-bug"></i> ${github.labels[0]}
+                    </span>
+
+                    ${github.labels[1] ? `
+                    <span class="bg-yellow-500 rounded-full py-1 px-5 flex gap-2 items-center text-sm">
+                    <i class="fa-solid fa-life-ring"></i> ${github.labels[1]}
+                    </span>
+                        ` : ''}
+
+                </div>
+
+
+                <div class="flex justify-between items-start text-sm text-gray-500 pt-4 ">
+                    <div class="flex flex-col space-y-2">
+                        <span>#1 by Jon Doe</span>
+                        <span>aSSIGN</span>
+                    </div>
+                     <div class="flex flex-col space-y-2">
+                         <span>01/05/2024</span>
+                        <span>01/55/2024</span>
+                    </div>
+
+                </div>
+            </div>
       
       `;
+
+        if (github.status == "open") {
+
+            githubCard.firstElementChild.classList.add('border-t-4', 'border-green-500');
+        } else {
+            githubCard.firstElementChild.classList.add('border-t-4', 'border-purple-700');
+
+        }
+
         githubContainer.append(githubCard);
     })
 }
 
- loadGithub();
+loadGithub();
